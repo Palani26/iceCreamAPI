78eab26d92588aff66b39c09e79e95d969a7fae8
palaniyappan ayyappan <payyappan@costco.com>
2021-12-15 11:37:23 -0800

    First push

---
+++ src/test/java/com/function/RatingTest.java
@@ -0,0 +1,57 @@
+package com.function;
+
+import com.microsoft.azure.functions.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+
+/**
+ * Unit test for Function class.
+ */
+@Disabled 
+public class RatingTest {
+    /**
+     * Unit test for HttpTriggerJava method.
+     */
+    @Disabled @Test
+    public void testHttpTriggerJava() throws Exception {
+        // Setup
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        final Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("input", "Azure");
+        doReturn(queryParams).when(req).getQueryParameters();
+
+        final Optional<String> queryBody = Optional.empty();
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        // Invoke
+        final HttpResponseMessage ret = new HttpResponseMessageMock(HttpStatus.OK,null,"Test");
+
+        // Verify
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+    }
+}
